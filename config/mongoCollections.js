72b78b690c0cfa4f74c655cfc5a23b99578af296
Mijeong Ban <mban1@stevens.edu>
2019-04-23 19:48:38 -0400

    initial commit

---
+++ config/mongoCollections.js
@@ -0,0 +1,16 @@
+const conn = require("./mongoConnection");
+
+const getCollectionFn = collection => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await conn();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+module.exports = {};
