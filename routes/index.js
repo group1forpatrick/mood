60ec539d0f1c50676b864a960b545e1244f8e277
Mijeong Ban <mban1@stevens.edu>
2019-05-03 17:22:08 -0400

    logout done for zipcode page

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const signupRoutes = require("./signup");
 const playlistRoutes = require("./playlists");
 const profileRoutes = require("./profile");
 const publicRoutes = require("./public");
+const logoutRoutes = require("./logout");
 
 const constructorMethod = app => {
   app.use("/private", privateRoutes);
   app.use("/playlists", playlistRoutes);
   app.use("/signup", signupRoutes);
   app.use("/profile", profileRoutes);
+  app.use("/logout", logoutRoutes);
   app.use("/", publicRoutes);
 
   app.use("*", (req, res) => {
