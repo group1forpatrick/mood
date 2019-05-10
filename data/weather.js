df07b685f2d8e4e9faf4a828366a07288bb520c6
Mario <malexan0@gmail.com>
2019-05-10 18:04:43 -0400

    Trying to get Spotify to work (Alex and Mario)

--- data/weather.js
+++ data/weather.js
@@ -1,26 +1,29 @@
 const axios = require("axios");
-const owm_api = "https://api.openweathermap.org/data/2.5/weather?zip={zip code}&units=imperial&APPID=49925dfe45be1e525f0ed1e2e01bc41f";
+const owm_api =
+  "https://api.openweathermap.org/data/2.5/weather?zip={zip code}&units=imperial&APPID=49925dfe45be1e525f0ed1e2e01bc41f";
 
 async function getWeather(zip) {
-    if(zip === undefined) throw "Must supply a zip code.";
-    if(typeof zip !== "string") throw "Zip Code must be a string.";
-    const addy = owm_api.replace("{zip code}", zip);
-    const {data} = await axios.get(addy).catch(function (e) {
-            console.log(`Weather: Error ${e.response.status}, ${e.response.data.message}`);
-    });
+  if (zip === undefined) throw "Must supply a zip code.";
+  if (typeof zip !== "string") throw "Zip Code must be a string.";
+  const addy = owm_api.replace("{zip code}", zip);
+  const { data } = await axios.get(addy).catch(function(e) {
+    console.log(
+      `Weather: Error ${e.response.status}, ${e.response.data.message}`
+    );
+  });
 
-    console.log(data);
-    return {
-        weather_tag: data.weather[0].main,
-        weather_des: data.weather[0].description,
-        max_temp:  Math.round(parseInt(data.main.temp_max)),
-        curr_temp:  Math.round(parseInt(data.main.temp)),
-        min_temp:  Math.round(parseInt(data.main.temp_min)),
-        city: data.name
-    };
+  //console.log(data);
+  return {
+    weather_tag: data.weather[0].main,
+    weather_des: data.weather[0].description,
+    max_temp: Math.round(parseInt(data.main.temp_max)),
+    curr_temp: Math.round(parseInt(data.main.temp)),
+    min_temp: Math.round(parseInt(data.main.temp_min)),
+    city: data.name
+  };
 }
 
-module.exports = {getWeather};
+module.exports = { getWeather };
 
 // async function main() {
 //     await getWeather("07300");
