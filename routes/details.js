690338e854b57ced8680abcd6fd6622282748e9d
Mijeong Ban <mban1@stevens.edu>
2019-05-14 15:27:42 -0400

    error message rendering done

--- routes/details.js
+++ routes/details.js
@@ -6,7 +6,6 @@ const playlistData = require("../data/playlists");
 router.get("/", async (req, res) => {
   const chosenPlaylist = xss(req.query.chosenPlaylist);
   const playlist = await playlistData.getPlaylists(chosenPlaylist);
-  console.log(playlist);
 
   try {
     res.render("private/details", { playlist: playlist });
