c05d289f5421ed8315e52ea10494f04e5a694b28
Mijeong Ban <mban1@stevens.edu>
2019-05-14 12:21:02 -0400

    profile basic setup

--- routes/details.js
+++ routes/details.js
@@ -5,7 +5,12 @@ const xss = require("xss");
 router.get("/", async (req, res) => {
   const chosenPlaylist = req.query.chosenPlaylist;
   console.log(chosenPlaylist);
-  res.render("private/details", {});
+
+  try {
+    res.render("private/details", {});
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 
 module.exports = router;
