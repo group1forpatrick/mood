16bfd9bcb4694c87bfad8e6955cb1c4258484db2
SecondThrow <malexan0@gmail.com>
2019-05-14 18:18:17 -0400

    Passing user data to profile route

--- routes/profile.js
+++ routes/profile.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const users = require("../data/users");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("private/profile", { user: req.session.user });
+    const user = await users.getUser(req.session.user._id);
+    res.render("private/profile", { user: user });
   } catch (e) {
     req.session.error = { status: 500, message: e };
     res.status(500).redirect("/");
