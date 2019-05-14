c18b9e99ee042f6c153dd1cdfcd0eda7fc15c94a
Mijeong Ban <mban1@stevens.edu>
2019-05-14 13:41:11 -0400

    icon added and footer fixed

--- routes/playlists.js
+++ routes/playlists.js
@@ -32,7 +32,6 @@ router.get("/", async (req, res) => {
     let lists = [];
     for (let element of removedUnliked)
       lists.push(await playlistsData.getPlaylists(element.spotifyId));
-
     res.render("private/playlists", {
       weather: weData,
       playlist: lists
